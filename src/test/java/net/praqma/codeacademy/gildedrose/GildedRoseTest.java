f3f0f41057d9430ffb4c63be5b3cfd9cba219562
Aneta Camo <AnetaCamova@gmail.com>
2017-08-17 15:49:26 +0200

    saying hi to fix the problem

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -1,3 +1,10 @@
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
+
 /* package net.praqma.codeacademy.gildedrose;
 
 import static org.junit.Assert.*;
